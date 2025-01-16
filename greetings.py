b5d175f07b613d459d17670455dec9e7e9976ec6
AlekceyKah <alekcey.v@gmail.com>
2025-01-16 13:52:56 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = 'Каширин Алексей' # username фамилия и имя студента
+title = 'Учебный проект' # title титульный "лист" проекта
+content = 'Работа с переменными' # content описание проекта
+status = 'Активна' # status статус проекта
+created_date = '13-01-2025' # created_date дата начала проекта
+issue_date = '12-01-2025' # issue_date предполагаемая дата окончания
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(issue_date)
+# print функция ввывода на экран
