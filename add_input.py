0a5873aa617da2c6a6c862b08a145b31e15f1857
AlekceyKah <alekcey.v@gmail.com>
2025-01-17 16:33:11 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,12 @@
+username = input('Введите фамилию: ') # username фамилия и имя студента
+title =  input('обозначение проекта: ') # title обозначение проекта 'Учебный проект'
+content =  input('описание проекта: ') # content описание проекта 'Работа с переменными'
+status =  input('статус проекта: ') # status статус проекта 'Активна'
+created_date =  input('дата начала проекта в формате день-месяц-год: ') # created_date дата начала проекта '13-01-2025'
+issue_date =  input('дата окончания проекта в формате день-месяц-год: ') # issue_date дата окончания проекта'13-02-2025'
+print(username) # print функция вывода на экран
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(issue_date)
