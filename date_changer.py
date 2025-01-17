210b646c84917651e794bebfd8defcb55ed9f1a7
AlekceyKah <alekcey.v@gmail.com>
2025-01-17 15:47:10 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,15 @@
+username = 'Каширин Алексей' # username фамилия и имя студента
+title = 'Учебный проект' # title титульный "лист" проекта
+content = 'Работа с переменными' # content описание проекта
+status = 'Активна' # status статус проекта
+created_date = '13-01-2025' # created_date дата начала проекта
+created_date_info = '13-01-2025'
+issue_date = '13-02-2025' # issue_date предполагаемая дата окончания
+issue_date_info = '13-02-2025'
+print(username) # print функция ввывода на экран
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(created_date_info[0:5]) # преобразование переменной с 1-ой строки по 5-ю
+print(created_date_info[:8]) # преобразование переменной с 1-ой строки по 8-ю
